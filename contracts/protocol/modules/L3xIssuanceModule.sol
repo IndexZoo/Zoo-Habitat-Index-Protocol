ac6ed25a7a71424da1647d6e621359f4fadf00e5
beber89 <mahmoudbaibars@gmail.com>
2022-01-26 16:01:27 +0200

    feat: Leverage ~ 3x achieved and tested (2.865)

--- contracts/protocol/modules/L3xIssuanceModule.sol
+++ contracts/protocol/modules/L3xIssuanceModule.sol
@@ -1,5 +1,5 @@
 /*
-    Copyright 2021 Index Zoo Ltd.
+    Copyright 2021 IndexZoo Ltd.
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -24,14 +24,14 @@ import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import { IController } from "../../interfaces/IController.sol";
-import { IDebtVault } from "../../interfaces/IDebtVault.sol";
+import { IUniswapV2Router } from "../../interfaces/external/IUniswapV2Router.sol";
+import { ILendingPool } from "../../interfaces/external/aave-v2/ILendingPool.sol";
 import { Invoke } from "../lib/Invoke.sol";
 import { ISetToken } from "../../interfaces/ISetToken.sol";
 import { IssuanceValidationUtils } from "../lib/IssuanceValidationUtils.sol";
 import { Position } from "../lib/Position.sol";
 import { ModuleBase } from "../lib/ModuleBase.sol";
-
-// TODO: Change comments
+import "hardhat/console.sol";
 
 /**
  * @title L3xIssuanceModule
@@ -41,47 +41,131 @@ import { ModuleBase } from "../lib/ModuleBase.sol";
  * including debt positions. Module hooks are added to allow for syncing of debt, 
  * to ensure debts are replicated correctly. 
  * 
+ * @dev Note that current state of the contract is for testing the feasibility of achieving ~ 3x Leverage
+ * @dev Next stage most of that logic will be invoked from the SetToken (or ZooToken)
+ * @dev SetToken will be changed as balance of (LevToken) (previously setToken) will be reflected by debt with AAVE 
+ * 
  * NOTE: 
  */
-contract L3xIssuanceModule is ModuleBase, ReentrancyGuard {
-    /// @notice Reference to debt vault 
-    /// @dev Acts as a storage for debt positions  
-    IDebtVault public vault;
+
+ // Dev Notes
+contract L3xIssuanceModule is  ReentrancyGuard {
+
+    ILendingPool public lender;
+    IUniswapV2Router public router;
+    IERC20 public weth; // 
+    IERC20 public dai;
 
 
 
     /* ============ Constructor ============ */
+    /**
+     * 
+     * FIXME: removed controller and moduleBase temporarily
+     * TODO: replace weth_ by underlying asset of LevToken (replacing SetToken)
+     * @param weth_   Address of WETH, represents baseToken (NOTE will be pointed at in SetToken)      
+     * @param dai_    Address of DAI, Represents the quoteToken 
+     */
     
-    constructor(IController _controller) public ModuleBase (_controller) {}
+    constructor(IERC20 weth_, IERC20 dai_) public  {
+        weth = weth_;
+        dai = dai_;
+    }
 
     /* ============ External Functions ============ */
 
+    function deposit(
+    )
+    external 
+    payable
+    {
+        payable(address(weth)).call{value: msg.value}("");
+    }
+
+
     /**
      * Mints Leverage token for investor
-     * Opens a position via perpetual protocol to provide leverage
-     * Update debt position for investor
+     * If setToken is bullish
+     * Deposits the base asset of that token on AAVE (i.e. WETH)
+     * Borrows quoteToken (i.e. DAI)
      *
-     * @param _setToken         Instance of the SetToken to issue
-     * @param _quantity         Quantity of SetToken to issue
-     * @param _to               Address to mint SetToken to
+     * TODO: put an argument for minimum quantity of token to receive from Issue (slippage)
+     * TODO: Leverage token is meant to be the object invoking the function trading calls
+     *
+     * @param _quantity         Quantity of quote token input to go long 
+     * @param _amountPerBase    amount of quote allowed to be borrowed per baseToken
      */
     function issue(
-        ISetToken _setToken,
+        // ISetToken _setToken,
         uint256 _quantity,
-        address _to
+        uint256 _amountPerBase
+        // address _to
     )
         external
         nonReentrant
-        onlyValidAndInitializedSet(_setToken)
+        // onlyValidAndInitializedSet(_setToken)
     {
-        // TODO: openPosition with perpMock
-        uint256 debtAmount = _quantity * 2;   // Leverage 3x -> debt is 2x
-        vault.addToDebt(_to, debtAmount);
+        dai.transferFrom(msg.sender, address(this), _quantity);
+        // swap dai for baseToken
+        uint256 price = 1000;  
+        uint256 amountOut = _swapQuoteForBase(_quantity, _quantity * 99/100/price);
+        // Borrow quoteToken from lending Protocol
+        uint256 borrowAmount = _borrowQuoteForBaseCollateral(amountOut, _amountPerBase);
+        amountOut = _swapQuoteForBase(borrowAmount, borrowAmount * 99/100/price);
+        borrowAmount = _borrowQuoteForBaseCollateral(amountOut, _amountPerBase);
+        amountOut = _swapQuoteForBase(borrowAmount, borrowAmount * 98/100/price);
+        require(amountOut != 0, "L3xIssueMod: Leveraging failed");
     }
 
-    function registerVault(ISetToken setToken_,  IDebtVault vault_) external onlySetManager(setToken_, msg.sender) {
-        vault = vault_;
+    function setLendingPool( ILendingPool lender_) external 
+    // onlySetManager(setToken_, msg.sender) 
+    {
+        lender = lender_;
+    }
+    function setRouter( IUniswapV2Router router_) external 
+    // onlySetManager(setToken_, msg.sender) 
+    {
+        router = router_;
     }
 
-    function removeModule() external override {}
+    // function removeModule() external override {}
+
+
+   /**  -------------------------------- Private functions --------------------------------------------------
+    */
+    function _borrowQuoteForBaseCollateral(
+        uint256 depositAmount,
+        uint256 borrowAmountPerBase
+    )
+    private
+    returns (uint256 notionalBorrowAmount)
+    {
+        // approve lender to receive swapped baseToken
+        weth.approve(address(lender), depositAmount);
+        lender.deposit(address(weth), depositAmount, address(this), 0);
+        notionalBorrowAmount = borrowAmountPerBase * depositAmount / (1 ether);
+        lender.borrow(address(dai), notionalBorrowAmount , 1, 0, address(this));
+        // TODO: ensure borrow took place ?
+    }
+    function _swapQuoteForBase(
+        uint256 amountIn, 
+        uint256 minAmountOut
+      ) 
+      private 
+      returns (uint256 amountOut) 
+    {
+        dai.approve(address(router),  amountIn);
+        address[] memory path = new address[](2);
+        path[0] = address(dai); 
+        path[1] = address(weth);
+        uint256[] memory amounts = router.swapExactTokensForTokens(
+            amountIn, 
+            minAmountOut, 
+            path, 
+            address(this), 
+           block.timestamp 
+        );
+        amountOut = amounts[1];
+        // TODO: ensure swap took place ?
+    }
 }
---
+++ test/l3xIssuanceModule.spec.ts
@@ -0,0 +1,158 @@
+import "module-alias/register";
+
+import { Account } from "@utils/test/types";
+import { ADDRESS_ZERO, MAX_UINT_256, ZERO } from "@utils/constants";
+import { 
+  StandardTokenMock,
+    UniswapV2Router02,
+} from "@utils/contracts";
+import {
+  getWaffleExpect,
+  getAccounts,
+    getAaveV2Fixture,
+    getUniswapFixture,//for aave
+} from "@utils/test/index";
+import { ethers } from "hardhat";
+import { bitcoin, ether } from "@utils/common/unitsUtils";
+
+//for aave
+import {  AaveV2Fixture } from "@utils/fixtures";
+
+import {BigNumber, Contract} from "ethers";
+import { WETH9__factory } from "@typechain/factories/WETH9__factory";
+import { L3xIssuanceModule } from "@typechain/L3xIssuanceModule";
+
+const expect = getWaffleExpect();
+const INTEGRATION_REGISTRY_RESOURCE_ID = 0;
+const initUniswapRouter = async(owner: Account, weth:  Contract, dai:  StandardTokenMock, btc: StandardTokenMock): Promise<UniswapV2Router02> => {
+      let router: UniswapV2Router02;
+
+         let uniswapFixture =  getUniswapFixture(owner.address);
+        await uniswapFixture.initialize(
+          owner,
+          weth.address,
+          btc.address,
+          dai.address
+        );
+        router = uniswapFixture.router;
+      await  weth.approve(router.address, MAX_UINT_256);
+      await dai.approve(router.address, MAX_UINT_256);
+      await router.addLiquidity(weth.address, dai.address, ether(5000), ether(5000000), ether(4990), ether(4900000), owner.address, MAX_UINT_256);
+      return router;
+}
+
+
+interface Accounts {
+  owner: Account;
+  protocolFeeRecipient: Account;
+  mockUser: Account;
+  mockSubjectModule: Account;
+  bob: Account;
+  alice: Account;
+  oscar: Account;
+  others: Account[];
+}
+
+interface Tokens {
+  weth: Contract;
+  mockDai: StandardTokenMock;
+  mockBtc: StandardTokenMock;
+}
+
+class Context {
+  public accounts= <Accounts>{};
+  public tokens = <Tokens> {};
+
+  public aaveFixture: AaveV2Fixture;
+  public subjectModule: L3xIssuanceModule;
+  public router: UniswapV2Router02;
+
+  // let StandardTokenMockContract: StandardTokenMock__factory;
+  // let L3xIssuanceModuleContract: L3xIssuanceModule__factory;
+  
+  public async initialize() : Promise<void>  {
+    [
+      this.accounts.owner,
+      this.accounts.protocolFeeRecipient,
+      this.accounts.mockUser,
+      this.accounts.mockSubjectModule,
+      this.accounts.bob,
+      this.accounts.alice,
+      this.accounts.oscar,
+      ...this.accounts.others
+    ] = await getAccounts();
+
+      this.aaveFixture = getAaveV2Fixture(this.accounts.owner.address);
+      this.tokens.mockDai =  await (await ethers.getContractFactory("StandardTokenMock")).deploy(this.accounts.owner.address, ether(100000000), "MockDai", "MDAI", 18);
+      this.tokens.mockBtc = await (await ethers.getContractFactory("StandardTokenMock")).deploy(this.accounts.owner.address, bitcoin(1000000), "MockBtc", "MBTC", 8);
+      this.tokens.weth = await new WETH9__factory(this.accounts.owner.wallet).deploy();
+
+      this.subjectModule = await (await ethers.getContractFactory("L3xIssuanceModule")).deploy(this.tokens.weth.address, this.tokens.mockDai.address);
+
+      await this.tokens.weth.connect(this.accounts.bob.wallet).deposit({value: ether(500)});
+      await this.tokens.weth.deposit({value: ether(5000)});
+      await this.tokens.weth.connect(this.accounts.mockSubjectModule.wallet).deposit({value: ether(500)});
+      // await subjectModule.deposit({value: ether(500)}); // subjectModule has 500 Weth
+      await this.tokens.mockDai.transfer(this.accounts.bob.address, ether(200000));
+      // await mockDai.transfer(mockSubjectModule.address, ether(200000));
+
+      this.router = await initUniswapRouter(this.accounts.owner, this.tokens.weth, this.tokens.mockDai, this.tokens.mockBtc);      
+      await  this.aaveFixture.initialize(this.tokens.weth.address, this.tokens.mockDai.address);
+      await this.subjectModule.setLendingPool(this.aaveFixture.lendingPool.address);
+      await this.subjectModule.setRouter(this.router.address);
+
+
+    // // Get the Protocol Data Provider
+    // let reserveDaiAddress  = (await aaveFixture.protocolDataProvider.getReserveTokensAddresses(mockDai.address))[1];   
+
+    
+    // // For stable debt tokens
+    // console.log(reserveDaiAddress);
+    // let reserveDai = (await ethers.getContractFactory("AaveV2StableDebtToken")).attach(reserveDaiAddress);
+    // console.log(reserveDai);
+    // await ethers.provider.  approveDelegation(bob.address, ether(8000));
+
+    // provide liquidity
+        await this.tokens.mockDai.connect(this.accounts.owner.wallet).approve(this.aaveFixture.lendingPool.address, MAX_UINT_256);
+        await this.aaveFixture.lendingPool.connect(this.accounts.owner.wallet).deposit(this.tokens.mockDai.address, ether(1000000), this.accounts.owner.address, ZERO);
+  }
+}
+
+describe("Controller", () => {
+  let ctx: Context;
+  beforeEach(async () => {
+
+  });
+
+  describe("Owner needs to deposit and withdraw collateral inside Aave", async function () {
+    beforeEach(async () => {
+      ctx = new Context();
+      await ctx.initialize();
+    });
+    it("Verify Interaction with Aave fixture directly - Borrow from aave and check debt", async ()=>{
+        await ctx.tokens.weth.connect(ctx.accounts.bob.wallet).approve(ctx.aaveFixture.lendingPool.address, MAX_UINT_256);
+        await ctx.tokens.weth.connect(ctx.accounts.mockSubjectModule.wallet).approve(ctx.aaveFixture.lendingPool.address, MAX_UINT_256);
+        await ctx.tokens.mockDai.connect(ctx.accounts.owner.wallet).approve(ctx.aaveFixture.lendingPool.address, MAX_UINT_256);
+
+        await ctx.aaveFixture.lendingPool.connect(ctx.accounts.mockSubjectModule.wallet).deposit(ctx.tokens.weth.address, ether(10), ctx.accounts.bob.address, ZERO);
+        await ctx.aaveFixture.lendingPool.connect(ctx.accounts.mockSubjectModule.wallet).deposit(ctx.tokens.weth.address, ether(10), ctx.accounts.mockSubjectModule.address, ZERO);
+        await ctx.aaveFixture.lendingPool.connect(ctx.accounts.owner.wallet).deposit(ctx.tokens.mockDai.address, ether(1000000), ctx.accounts.owner.address, ZERO);
+        await ctx.aaveFixture.lendingPool.connect(ctx.accounts.mockSubjectModule.wallet).borrow(ctx.tokens.mockDai.address, ether(8000), BigNumber.from(1), ZERO, ctx.accounts.mockSubjectModule.address);
+       
+        let userData = (await ctx.aaveFixture.lendingPool.getUserAccountData(ctx.subjectModule.address));
+        expect(userData.healthFactor).to.gt(ether(1));  // ~ 1.03 ETH 
+    });
+    it("SubjectModule deposits weth and borrows against it", async ()=>{
+       await ctx.tokens.mockDai.approve(ctx.subjectModule.address, ether(10000));
+       await ctx.subjectModule.issue(ether(10000), ether(700));
+
+       let userData = (await ctx.aaveFixture.lendingPool.getUserAccountData(ctx.subjectModule.address));
+       let leverage = userData.totalCollateralETH.add(userData.totalDebtETH).mul(1000).div(ether(10));
+
+       expect(leverage).to.be.gt(2500);   // leverage = 2865 for minimum healthFactor/maximum risk
+       expect(userData.currentLiquidationThreshold).to.gt(BigNumber.from(8000));
+       expect(userData.healthFactor).to.gt(ether(1));  // ~ 1.03 ETH 
+    });
+
+  });
+});
